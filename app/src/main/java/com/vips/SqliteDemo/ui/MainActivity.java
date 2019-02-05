f975cc62c4c46be21cfcc2fcc0c550eb8b4def8c
Vipul Damor <vipul.damor@eryushion.com>
2019-02-05 11:04:53 +0530

    init commit

--- app/src/main/java/com/vips/SqliteDemo/ui/MainActivity.java
+++ app/src/main/java/com/vips/SqliteDemo/ui/MainActivity.java
@@ -8,6 +8,7 @@ import com.vips.SqliteDemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
